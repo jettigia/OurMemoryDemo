0a82b8a5aa8ddee713da3a90cda1595aa727fcb1
FiniteRealityTech <35709683+FiniteRealityTech@users.noreply.github.com>
2019-07-23 21:58:17 -0400

    Added context and repository layers. Reorganized file structure slightly.

---
+++ OurMemory/OurMemory/OurMemory/Data/Repositories/IDataRepository.cs
@@ -0,0 +1,22 @@
+﻿using OurMemory.Data.Entities;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurMemory.Data.Repositories
+{
+    public interface IDataRepository<DataType>
+        where DataType : class, IOurMemoryBase
+    {
+        Task Create(DataType entity);
+
+        Task Delete(int id);
+
+        IQueryable<DataType> GetAll();
+
+        Task<DataType> GetById(int id);
+
+        Task<DataType> Read(params int[] key);
+
+        Task Update(int id, DataType entity);
+    }
+}
