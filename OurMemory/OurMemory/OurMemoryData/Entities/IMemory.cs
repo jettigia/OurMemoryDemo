b74d0298c9188ec71cd5c6cd85c68e68627e83a5
Ralph Maurmeier <ralph.maurmeier@clarkepsi.com>
2019-09-21 18:53:18 -0400

    Removed most projects.

--- OurMemory/OurMemory/OurMemoryData/Entities/IMemory.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OurMemoryData.Entities
-{
-    public interface IMemory
-    {
-        int Id { get; set; }
-        string Title { get; set; }
-        string Description { get; set; }
-    }
-}
