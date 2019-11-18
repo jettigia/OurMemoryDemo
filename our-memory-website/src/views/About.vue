2765a3b2bc873cd8ef3a63336e89f1582ea65939
Ralph Maurmeier <ralph.maurmeier@ingagepartners.com>
2019-11-18 02:23:11 -0500

    Brian's additions.

--- our-memory-website/src/views/About.vue
+++ our-memory-website/src/views/About.vue
@@ -1,8 +1,43 @@
 <template>
+
   <div class="about">
-    <h1>About Our Memory</h1>
-    <p>
-      Our Memory is dedicated to storing and protecting your memories.
-    </p>
+    <div id="main-page">
+    
+    <!-- About section  -->
+	<section class="about-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-lg-6 p-0">
+					<div class="about-bg set-bg"><img src="../assets/faviconORIG.png"></div>
+				</div>
+				<div class="col-lg-6 p-0">
+					<div class="about-text">
+						<h2>About Our Memory</h2>
+						<p>Our Memory is dedicated to storing and protecting your memories. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas ac-cumsan lacus vel facilisis commodo viverra maecenas. Lorem ipsum dolor sit amet, consectetur adipis cing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum susp endisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.</p>
+						<h4>Info 1</h4>
+						<p>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas ac-cusan. </p>
+						<h4>Info 2</h4>
+						<p>Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas ac-cusan lacus vel facilisis.</p>
+						<a href="#" class="site-btn">contact us</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- About section end  -->
+
+  </div>
   </div>
 </template>
+
+
+<style scoped>
+
+  .about {
+    /* background-image: url("../assets/home-background-image.jpg");
+    background-size: cover; */
+    height: 90%;
+    /* padding-bottom: 0px 0px 19px 0px; */
+  }
+
+</style>
--- our-memory-website/src/views/Home.vue
+++ our-memory-website/src/views/Home.vue
@@ -1,10 +1,29 @@
 <template>
+
   <div class="home home-background">
-    <img alt="Our Memory logo" src="../assets/logo.png" height="5%" width="5%" />
-    <img alt="Our Memory background" src="../assets/home-background-image.jpg" height="25%" width="25%" />
-    <body>Welcome!</body>
+    <div id="main-page">
+          
+	<div id="center-home-panel" class="container text-center">
+		<div class="row">
+
+			<div class="col-md-12 col-sm-12">
+				<h3 class="wow bounceIn" data-wow-delay="0.9s">Welcome Summoner!</h3>
+				<h1 class="wow fadeInUp" data-wow-delay="1.6s">Bloob de bloob:</h1>
+				<h4></h4>
+				<h5 class="wow fadeInUp" data-wow-delay="2s">Boop doop de doop, loop de scoop and doop de boop!</h5>
+        <br>
+        <router-link to="/about" class="btn btn-lg btn-default smoothScroll wow fadeInUp ">LEARN MORE</router-link>
+        <router-link to="/registration" class="btn btn-lg btn-danger smoothScroll wow fadeInUp">Try Now</router-link>
+			</div>
+
+    </div>
+		</div>
+	</div>
+	
   </div>
 </template>
+
+//SCRIPTS -----------------------------------------------------------------
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
@@ -15,9 +34,32 @@ import { Component, Vue } from "vue-property-decorator";
 export default class Home extends Vue {}
 </script>
 
+//STYLE -----------------------------------------------------------------
+
 <style scoped>
-home-background {
-  background-image: url("/src/assets/home-background-image.jpg");
+home-html {
+  /* height: 100%; */
   background-color: #cccccc;
+}
+.home {
+  background-image: url("../assets/home-background-image.jpg");
+  background-size: cover;
+  height: 90%;
+  /* padding-bottom: 0px 0px 19px 0px; */
+}
+#center-home-panel {
+  
+  position: absolute;
+  margin: auto;
+     
+  right: 0;
+  left: 0;
+  bottom: 30%;
+  top: 35%;
+
+  width: 70%;
+  padding: 30px;
+  background-color: #ffffffef;
+  
 }
 </style>
