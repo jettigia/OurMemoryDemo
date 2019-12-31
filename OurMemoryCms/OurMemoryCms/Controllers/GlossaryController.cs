593a0687ccb1ee0a21221b21c1938715a8082f05
FiniteRealityTech <35709683+FiniteRealityTech@users.noreply.github.com>
2019-12-31 11:19:59 -0500

    Created Api. Started Glossary tutorial. Ready to add authentication.

---
+++ OurMemoryCms/OurMemoryCms/Controllers/GlossaryController.cs
@@ -0,0 +1,94 @@
+﻿// https://auth0.com/blog/how-to-build-and-secure-web-apis-with-aspnet-core-3/
+
+using Microsoft.AspNetCore.Mvc;
+using OurMemoryWebApi.Data;
+using OurMemoryWebApi.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OurMemoryWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GlossaryController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post(GlossaryModel glossaryItem)
+        {
+            var existingGlossaryItem = Glossary.Data
+                
+                .Find(item =>
+                    item.Term.Equals(glossaryItem.Term, StringComparison.InvariantCultureIgnoreCase));
+
+            if (existingGlossaryItem != null)
+            {
+                return Conflict("Cannot create the term because it already exists.");
+            }
+            else
+            {
+                Glossary.Data.Add(glossaryItem);
+                var resourceUrl = Path.Combine(Request.Path.ToString(), Uri.EscapeUriString(glossaryItem.Term));
+                return Created(resourceUrl, glossaryItem);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<List<GlossaryModel>> Get()
+        {
+            return Ok(Glossary.Data);
+        }
+
+        [HttpGet]
+        [Route("{term}")]
+        public ActionResult<GlossaryModel> Get(string term)
+        {
+            var glossaryItem = Glossary.Data.Find(item =>
+                    item.Term.Equals(term, StringComparison.InvariantCultureIgnoreCase));
+
+            if (glossaryItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(glossaryItem);
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put(GlossaryModel    glossaryItem)
+        {
+            var existingGlossaryItem = Glossary.Data.Find(item =>
+            item.Term.Equals(glossaryItem.Term, StringComparison.InvariantCultureIgnoreCase));
+
+            if (existingGlossaryItem == null)
+            {
+                return BadRequest("Cannot update a non-existing term.");
+            }
+            else
+            {
+                existingGlossaryItem.Definition = glossaryItem.Definition;
+                return Ok();
+            }
+        }
+
+        [HttpDelete]
+        [Route("{term}")]
+        public ActionResult Delete(string term)
+        {
+            var glossaryItem = Glossary.Data.Find(item =>
+                   item.Term.Equals(term, StringComparison.InvariantCultureIgnoreCase));
+
+            if (glossaryItem == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Glossary.Data.Remove(glossaryItem);
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ OurMemoryCms/OurMemoryCms/Data/Glossary.cs
@@ -0,0 +1,26 @@
+﻿using OurMemoryWebApi.Models;
+using System.Collections.Generic;
+
+namespace OurMemoryWebApi.Data
+{
+    public static class Glossary
+    {
+        public static List<GlossaryModel> Data = new List<GlossaryModel> {
+            new GlossaryModel
+            {
+                Term= "Access Token",
+                Definition = "A credential that can be used by an application to access an API. It informs the API that the bearer of the token has been authorized to access the API and perform specific actions specified by the scope that has been granted."
+            },
+            new GlossaryModel
+            {
+                Term= "JWT",
+                Definition = "An open, industry standard RFC 7519 method for representing claims securely between two parties. "
+            },
+            new GlossaryModel
+            {
+                Term= "OpenID",
+                Definition = "An open standard for authentication that allows applications to verify users are who they say they are without needing to collect, store, and therefore become liable for a user’s login information."
+            }
+        };
+    }
+}
