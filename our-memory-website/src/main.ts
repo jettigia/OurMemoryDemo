d8b60438953cc983fde7e3f73069c2c239f82e43
FiniteRealityTech <35709683+FiniteRealityTech@users.noreply.github.com>
2020-01-28 12:15:48 -0500

    Finished adding user store.

--- our-memory-website/src/main.ts
+++ our-memory-website/src/main.ts
@@ -1,5 +1,7 @@
 import Vue from "vue";
-import App from "@/App.vue";
+import Vuex from "vuex";
+import store from "./store/index.js";
+import App from "./App.vue";
 import router from "./router";
 import "./registerServiceWorker";
 import BootstrapVue from "bootstrap-vue";
@@ -8,8 +10,10 @@ import "bootstrap-vue/dist/bootstrap-vue.css";
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+Vue.use(Vuex);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
---
+++ our-memory-website/src/store/modules/user.js
@@ -0,0 +1,71 @@
+import UserService from "../../components/user-service.js";
+
+const state = {
+  user: null
+};
+
+// getters
+const getters = {
+  user: state => {
+    return state.user;
+  }
+};
+
+// actions
+const actions = {
+  async login({ commit, state }, user) {
+    const userService = new UserService();
+
+    var result = await userService
+      .authenticate(user)
+      .then(response => {
+        commit("setUser", response.data);
+      })
+      .catch(error => {
+        commit("setLoginError", error.response.data.errors);
+      });
+  },
+  async register({ commit, state }, user) {
+    const userService = new UserService();
+
+    var result = await userService
+      .register(user)
+      .then(response => {
+        debugger;
+        commit("setRegisterSuccess");
+      })
+      .catch(error => {
+        debugger;
+        commit("setRegisterFail", error.response.data.errors);
+      });
+    debugger;
+  }
+};
+
+// mutations
+const mutations = {
+  setLoginError(state, user) {
+    state.user.loginError = user;
+  },
+  setRegisterFail(state, user) {
+    state.user.registerStatus = false;
+    state.user.registerError = user;
+  },
+  setRegisterSuccess(state) {
+    debugger;
+    state.user = {};
+    state.user.registerStatus = true;
+    debugger;
+  },
+  setUser(state, user) {
+    state.user = user;
+  }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations
+};
