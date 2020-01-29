adab38e5025e7457075b68dd26b6cdd80e3fdb67
FiniteRealityTech <35709683+FiniteRealityTech@users.noreply.github.com>
2020-01-28 19:12:42 -0500

    zstarted adding store procedures to registration.

--- our-memory-website/src/views/Registration.vue
+++ our-memory-website/src/views/Registration.vue
@@ -1,333 +1,98 @@
 <template>
-  <div class="home home-background">
-    <div id="main-page">
-      <div id="center-home-panel" class="container text-center">
-        <div
-          id="modalImage1"
-          class="modal fade"
-          tabindex="-1"
-          role="dialog"
-          aria-labelledby="myModalLabel"
-          aria-hidden="true"
+  <div class="registration" style="text-align: center;">
+    <h1>Registration</h1>
+    <div style="width: 50%; left: 25%; display: inline-block">
+      <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+        <b-form-group
+          id="emailLabel"
+          label="Email address:"
+          label-for="emailInput"
+          description="We'll never share your email with anyone else."
         >
-          <div class="modal-dialog">
-            <div class="modal-content">
-              <div class="modal-header">
-                <button
-                  type="button"
-                  class="close font-weight-light"
-                  data-dismiss="modal"
-                  aria-hidden="true"
-                >
-                  ×
-                </button>
-              </div>
-              <div class="modal-body">
-                <!-- Terms o service -->
-                <div>
-                  <div style="left:67.34px;top:52.56px" class="cls_002">
-                    <span class="cls_002">OurMemory Terms and Conditions</span>
-                  </div>
-                  <div style="left:53.88px;top:128.66px" class="cls_004">
-                    <span class="cls_004">Terms and Conditions</span>
-                  </div>
-                  <div style="left:53.16px;top:170.06px" class="cls_003">
-                    <span class="cls_003">Last updated: 1/17/2020</span>
-                  </div>
-                  <div style="left:53.16px;top:205.82px" class="cls_003">
-                    <span class="cls_003"
-                      >Please read these Terms and Conditions carefully before
-                      using</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:220.46px" class="cls_003">
-                    <span class="cls_003">the </span
-                    ><A HREF="http://www.ourmemory.com/"
-                      >http://www.ourmemory.com</A
-                    >
-                    website and the OurMemory mobile<span class="cls_005">
-                    </span
-                    ><span class="cls_003">application (the "Service")</span>
-                  </div>
-                  <div style="left:53.64px;top:235.22px" class="cls_003">
-                    <span class="cls_003">operated by OurMemory</span
-                    ><span class="cls_005">.</span>
-                  </div>
-                  <div style="left:53.16px;top:271.13px" class="cls_003">
-                    <span class="cls_003"
-                      >Your access to and use of the Service is conditioned on
-                      your acceptance of and compliance with</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:285.77px" class="cls_003">
-                    <span class="cls_003"
-                      >these Terms. These Terms apply to all visitors, users and
-                      others who access or use the Service.</span
-                    >
-                  </div>
-                  <div style="left:53.16px;top:315.77px" class="cls_006">
-                    <span class="cls_006"
-                      >By accessing or using the Service you agree to be bound
-                      by these Terms. If you disagree</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:329.33px" class="cls_006">
-                    <span class="cls_006"
-                      >with any part of the terms then you may not access the
-                      Service.</span
-                    >
-                  </div>
-                  <div style="left:53.16px;top:359.21px" class="cls_006">
-                    <span class="cls_006">Content</span>
-                  </div>
-                  <div style="left:53.16px;top:389.21px" class="cls_003">
-                    <span class="cls_003"
-                      >Our Service allows you to post, link, store, share and
-                      otherwise make available certain information,</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:403.85px" class="cls_003">
-                    <span class="cls_003"
-                      >text, graphics, videos, or other material
-                      ("Content").</span
-                    >
-                  </div>
-                  <div style="left:53.16px;top:442.63px" class="cls_003">
-                    <span class="cls_003"
-                      >Our Service may contain links to third-party web sites or
-                      services that are not owned or controlled</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:457.39px" class="cls_003">
-                    <span class="cls_003">by OurMemory.</span>
-                  </div>
-                  <div style="left:53.16px;top:493.27px" class="cls_003">
-                    <span class="cls_003"
-                      >OurMemory has no control over, and assumes no
-                      responsibility for, the content, privacy policies,
-                      or</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:511.39px" class="cls_003">
-                    <span class="cls_003"
-                      >practices of any third party web sites or services. You
-                      further acknowledge and agree that</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:529.51px" class="cls_003">
-                    <span class="cls_003"
-                      >OurMemory shall not be responsible or liable, directly or
-                      indirectly, for any damage or loss caused</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:547.63px" class="cls_003">
-                    <span class="cls_003"
-                      >or alleged to be caused by or in connection with use of
-                      or reliance on any such content, goods or</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:565.75px" class="cls_003">
-                    <span class="cls_003"
-                      >services available on or through any such web sites or
-                      services.</span
-                    >
-                  </div>
-                  <div style="left:53.16px;top:599.26px" class="cls_006">
-                    <span class="cls_006">Changes</span>
-                  </div>
-                  <div style="left:53.16px;top:629.26px" class="cls_003">
-                    <span class="cls_003"
-                      >We reserve the right, at our sole discretion, to modify
-                      or replace these Terms at any time. If a</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:647.26px" class="cls_003">
-                    <span class="cls_003"
-                      >revision is material we will try to provide at least 15
-                      days' notice prior to any new terms taking effect.</span
-                    >
-                  </div>
-                  <div style="left:53.64px;top:665.14px" class="cls_003">
-                    <span class="cls_003"
-                      >What constitutes a material change will be determined at
-                      our sole discretion.</span
-                    >
-                  </div>
-                  <div style="left:53.16px;top:698.26px" class="cls_006">
-                    <span class="cls_006">Contact Us</span>
-                  </div>
-                  <div style="left:53.16px;top:728.14px" class="cls_003">
-                    <span class="cls_003"
-                      >If you have any questions about these Terms, please
-                      contact us.</span
-                    >
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+          <b-form-input
+            id="emailInput"
+            v-model="model.email"
+            type="email"
+            required
+            placeholder="Enter email"
+          ></b-form-input>
+        </b-form-group>
 
-        <div class="registration" style="text-align: center;">
-          <h1>Registration</h1>
-          <br />
-          <div class="regBody">
-            <b-form @submit="onSubmit" @reset="onReset" v-if="show">
-              <b-form-group
-                id="emailLabel"
-                label="Email address:"
-                label-for="emailInput"
-                description="We'll never share your email with anyone else."
-              >
-                <b-form-input
-                  id="emailInput"
-                  v-model="model.email"
-                  type="email"
-                  required
-                  placeholder="Enter email"
-                ></b-form-input>
-              </b-form-group>
+        <b-form-group
+          id="firstName.Label"
+          label="First Name:"
+          label-for="firstNameInput"
+        >
+          <b-form-input
+            id="firstNameInput"
+            v-model="model.firstName"
+            required
+            placeholder="Enter first name"
+          ></b-form-input>
+        </b-form-group>
 
-              <div style="display:inline;">
-                <b-form-group
-                  id="firstName.Label"
-                  label="First Name:"
-                  label-for="firstNameInput"
-                  style="display:inline;float:left;width:48%;"
-                >
-                  <b-form-input
-                    id="firstNameInput"
-                    v-model="model.firstName"
-                    required
-                    placeholder="Enter first name"
-                  ></b-form-input>
-                </b-form-group>
-                <b-form-group
-                  id="lastName.Label"
-                  label="Last Name:"
-                  label-for="lastNameInput"
-                  style="display:inline;float:right;width:48%;"
-                >
-                  <b-form-input
-                    id="lastNameInput"
-                    v-model="model.lastName"
-                    required
-                    placeholder="Enter last name"
-                  ></b-form-input>
-                </b-form-group>
-              </div>
+        <b-form-group
+          id="lastName.Label"
+          label="Last Name:"
+          label-for="lastNameInput"
+        >
+          <b-form-input
+            id="lastNameInput"
+            v-model="model.lastName"
+            required
+            placeholder="Enter last name"
+          ></b-form-input>
+        </b-form-group>
 
-              <b-form-group
-                id="username.Label"
-                label="Username:"
-                label-for="username"
-                :invalid-feedback="usernameError"
-              >
-                <b-form-input
-                  id="username"
-                  v-model="model.username"
-                  required
-                  placeholder="Enter username 8 to 16 characters"
-                ></b-form-input>
-                <div :v-show="error">{{ model.usernameError }}</div>
-              </b-form-group>
+        <b-form-group
+          id="username.Label"
+          label="Username:"
+          label-for="username"
+        >
+          <b-form-input
+            id="username"
+            v-model="model.username"
+            required
+            placeholder="Enter username"
+          ></b-form-input>
+        </b-form-group>
 
-              <div style="display:inline;">
-                <b-form-group
-                  id="passwordLabel"
-                  label="Password:"
-                  label-for="passwordInput"
-                  style="display:inline;float:left;width:48%;"
-                >
-                  <b-form-input
-                    id="passwordInput"
-                    v-model="model.password"
-                    required
-                    placeholder="Enter password with one uppercase, lowercase letter and one digit"
-                    type="password"
-                  ></b-form-input>
-                  <div :v-show="error">{{ model.passwordError }}</div>
-                </b-form-group>
+        <b-form-group
+          id="passwordLabel"
+          label="password:"
+          label-for="passwordInput"
+        >
+          <b-form-input
+            id="passwordInput"
+            v-model="model.password"
+            required
+            placeholder="Enter password"
+            type="password"
+          ></b-form-input>
+        </b-form-group>
 
-                <b-form-group
-                  id="confirmPasswordLabel"
-                  label="Confirm password:"
-                  label-for="ConfirmPasswordInput"
-                  style="display:inline;float:right;width:48%;"
-                  :invalid-feedback="passwordError"
-                  :state="hasError"
-                  ><b-form-input
-                    id="confirmPasswordInput"
-                    v-model="model.confirmPassword"
-                    required
-                    placeholder="Confirm password"
-                    type="password"
-                  ></b-form-input>
-                </b-form-group>
-              </div>
+        <b-form-group
+          id="confirmpasswordLabel"
+          label="Confirm password:"
+          label-for="input-3"
+          ><b-form-input
+            id="confirmpasswordInput"
+            v-model="model.confirmPassword"
+            required
+            placeholder="Confirm password"
+            type="password"
+          ></b-form-input>
+        </b-form-group>
 
-              <br />
-
-              <b-button
-                style="width:100%;"
-                class="btn btn-lg btn-default smoothScroll wow fadeInUp"
-                type="submit"
-                variant="warning"
-                >Sign up</b-button
-              >
-
-              <br /><br />
-              <p>
-                By signing up, you agree to our<br />
-                <a href="#" data-toggle="modal" data-target="#modalImage1"
-                  >Terms & Privacy Policy</a
-                >
-              </p>
-              <!-- <b-button class="btn btn-lg btn-default smoothScroll wow fadeInUp" type="reset" variant="danger">Reset</b-button> -->
-            </b-form>
-          </div>
-        </div>
-      </div>
+        <b-button type="submit" variant="primary">Submit</b-button>
+        <b-button type="reset" variant="danger">Reset</b-button>
+      </b-form>
     </div>
   </div>
 </template>
 
-<style scoped>
-home-html {
-  /* height: 100%; */
-  background-color: #cccccc;
-}
-.home {
-  height: 90%;
-  /* padding-bottom: 0px 0px 19px 0px; */
-}
-#center-home-panel {
-  position: absolute;
-  margin: auto;
-  right: 0;
-  left: 0;
-  top: 10%;
-  width: 70%;
-  padding: 30px;
-  background-color: #ffffffef;
-}
-.regBody {
-  width: 50%;
-  left: 25%;
-  display: inline-block;
-}
-
-@media only screen and (max-width: 600px) {
-  .regBody {
-    width: 100%;
-    left: 25%;
-    display: inline-block;
-  }
-}
-</style>
-
 <script>
-import UserService from "@/components/user-service";
+import { mapActions, mapState } from "vuex";
 
 export default {
   data() {
@@ -337,68 +102,48 @@ export default {
         firstName: "",
         lastName: "",
         username: "",
-        usernameError: "",
         confirmPassword: "",
-        password: "",
-        passwordError: ""
+        password: ""
       },
-      show: true,
-      error: false
+      show: true
     };
   },
   methods: {
+    ...mapActions({
+      register: "user/register"
+    }),
     async onSubmit(evt) {
       evt.preventDefault();
-      this.errorReset();
-      let that = this;
 
-      var userService = new UserService();
-      var result = await userService
-        .register({
-          email: this.model.email,
-          firstName: this.model.firstName,
-          lastName: this.model.lastName,
-          username: this.model.username,
-          password: this.model.password
-        })
-        .then(response => {
-          that.$router.push("registrationSuccess");
-        })
-        .catch(error => {
-          if (error.response.status == 400) {
-            that.error = true;
-            if (error.response.data.errors.Password) {
-              that.model.passwordError = error.response.data.errors.Password.join(
-                " "
-              );
-            }
-            if (error.response.data.errors.Username) {
-              that.model.usernameError = error.response.data.errors.Username.join(
-                " "
-              );
-            }
-          } else {
-            that.$router.push("registrationUnsuccess");
-          }
-        });
-    },
-    errorReset() {
-      this.model.passwordError = "";
-      this.model.usernameError = "";
-      this.error = false;
+      await this.register({
+        email: this.model.email,
+        firstName: this.model.firstName,
+        lastName: this.model.lastName,
+        username: this.model.username,
+        password: this.model.password
+      });
+
+      if (this.user.registerStatus) {
+      } else {
+        // error labels missing.
+      }
     },
     onReset(evt) {
       evt.preventDefault();
       // Reset our form values
       this.model.email = "";
       this.model.name = "";
-      this.errorReset();
       // Trick to reset/clear native browser form validation state
       this.show = false;
       this.$nextTick(() => {
         this.show = true;
       });
     }
+  },
+  computed: {
+    ...mapState({
+      user: state => state.user.user
+    })
   }
 };
 </script>
